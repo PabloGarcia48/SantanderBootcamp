e139a0f706485f3160a895b55120b0ae2b8d7bee
PabloGarcia48 <pabloroberto2011@gmail.com>
2022-06-29 12:49:34 -0300

    Criadas listas com mapOf

--- 3_List/src/main/kotlin/one/digitalinnovation/collections/8_TestMap.kt
+++ 3_List/src/main/kotlin/one/digitalinnovation/collections/8_TestMap.kt
@@ -4,5 +4,12 @@ fun main() {
     val pair: Pair<String, Double> = Pair("João", 1000.0)
     val map1 = setOf(pair)
 
-    map1.forEach{ (x, y) -> println("Chave: $x    Valor: $y")}
+    map1.forEach { (x, y) -> println("Chave: $x    Valor: $y") }
+
+    val map2 = mapOf(
+        "Pedro" to 2000.0,
+        "Maria" to 3000.0
+    )
+    println("------------------")
+    map2.forEach { (x, y) -> println("Chave: $x    Valor: $y") }
 }
