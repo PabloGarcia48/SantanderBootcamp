63bfc5da155d14499bb93f9c9635407c8f7a23b2
PabloGarcia48 <pabloroberto2011@gmail.com>
2022-06-30 12:53:20 -0300

    Mutable List

--- 3_List/src/main/kotlin/one/digitalinnovation/collections/9_TesteColectionsMutables.kt
+++ 3_List/src/main/kotlin/one/digitalinnovation/collections/9_TesteColectionsMutables.kt
@@ -8,4 +8,26 @@ fun main() {
     val funcionarios = mutableListOf(joao, maria)
     funcionarios.forEach { (println(it)) }
 
+    println("--- adição do Pedro---")
+    funcionarios.add(pedro)
+    funcionarios.forEach { (println(it)) }
+
+    println("--- remoção do João---")
+    funcionarios.remove(joao)
+    funcionarios.forEach { (println(it)) }
+
+    println("--- SET ---")
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach { (println(it)) }
+
+    println("--- adicionado ao SET Pedro e Maria ---")
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { (println(it)) }
+
+    println("--- removido do SET a Maria ---")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach { (println(it)) }
+
+
 }
